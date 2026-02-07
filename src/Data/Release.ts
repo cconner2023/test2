29247c1626286a2db85e245d5416220241501999
cconner2023 <chconnor7361@gmail.com>
2026-02-07 10:31:30 +0100

    update release notes

--- src/Data/Release.ts
+++ src/Data/Release.ts
@@ -6,8 +6,10 @@ export interface ReleaseNoteTypes {
 }
 
 export const ReleaseNotes: ReleaseNoteTypes[] = [
-    { version: '2.6.1', type: 'changed', text: 'Improved UI desktop and mobile' },
-    { version: '2.6.0', type: 'bug', text: 'Responsive UI desktop / mobile' },
+    { version: '2.6.1', type: 'changed', text: 'decision making modal / drawer' },
+    { version: '2.6.1', type: 'added', text: 'user login' },
+    { version: '2.6.1', type: 'bug', text: 'modal backdrop correction' },
+    { version: '2.6.0', type: 'bug', text: 'Responsive desktop / mobile' },
     { version: '2.6.0', type: 'changed', text: 'settings modal / drawer' },
     { version: '2.6.0', type: 'added', text: 'dark | light mode toggle' },
     { version: '2.6.0', type: 'added', text: 'Export note as shared content: QR code, 2D barcode, encoded text string' },
