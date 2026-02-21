e33f92235f0fd73e1c235ea13fcee08b5ccb5a03
PackageBackEnd Initializer <autoforge@packagebackend.local>
2026-02-21 12:10:14 +0100

    update release

--- src/Data/Release.ts
+++ src/Data/Release.ts
@@ -8,13 +8,11 @@ export interface ReleaseNoteTypes {
 
 export const ReleaseNotes: ReleaseNoteTypes[] = [
     { version: '2.6.3', type: 'added', text: 'text expander for HPI and physical exams. Make your own templates' },
+    { version: '2.6.3', type: 'added', text: 'clinic heirarchy. Parent clinics see child clinic documentation for easier patient tracking' },
     { version: '2.6.3', type: 'added', text: 'DD FORM 689 export' },
-    { version: '2.6.3', type: 'added', text: 'physical exam section and HPI can be toggled on in settings' },
-    { version: '2.6.3', type: 'changed', text: 'tutorials and how-to. Found in settings' },
+    { version: '2.6.2', type: 'added', text: 'physical exam section and HPI can be toggled on in settings' },
+    { version: '2.6.2', type: 'changed', text: 'tutorials and how-to. Found in settings' },
     { version: '2.6.2', type: 'started', text: 'training list and evaluation through the supervisor role' },
     { version: '2.6.1', type: 'added', text: 'biometric/pin for timeout (optional)' },
     { version: '2.6.1', type: 'added', text: 'User login and UIC assignment - see all the notes from your clinic wherever you log in' },
-    { version: '2.6.0', type: 'started', text: 'training documentation pushed to server.' },
-    { version: '2.6.0', type: 'changed', text: 'Import note from other devices via barcode' },
-    { version: '2.6.0', type: 'added', text: 'share, copy, save your notes locally after creating them' },
 ]
