f7fd80c61505e493ba597cfff7166af33301a8cf
PackageBackEnd Initializer <autoforge@packagebackend.local>
2026-03-22 11:55:38 +0100

    Updates

---
+++ assets/ContentWrapper-BhpfNZ-D.js
@@ -0,0 +1,1 @@
+import{j as l}from"./index-DSTwvSmB.js";const n=({children:s,slideDirection:t,swipeHandlers:e})=>{const i={"":"",left:"animate-slide-in-left",right:"animate-slide-in-right"};return l.jsx("div",{className:`h-full w-full ${i[t]}`,...e,children:s})};export{n as C};
